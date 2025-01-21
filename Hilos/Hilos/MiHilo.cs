8b2919e9720c9be3303e338ee39b10da5a3c9ba7
Daniel <danielhgomez2005@gmail.com>
2025-01-21 16:44:45 +0100

    Add files via upload

---
+++ Hilos/Hilos/MiHilo.cs
@@ -0,0 +1,41 @@
+﻿namespace Hilos;
+
+    public class MiHilo<T> where T : struct
+    {
+        private Thread hilo;
+        private string text;
+        private RefWrapper<T> valor;
+        
+        // Evento a nivel de instancia en lugar de estático
+        public event Action OnComplete;
+
+        public MiHilo(string text, RefWrapper<T> valor)
+        {
+            this.text = text;
+            this.valor = valor;
+            hilo = new Thread(_process);
+        }
+
+        public void Start()
+        {
+            hilo.Start();
+        }
+
+        private void _process()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(text);
+                // Aquí puedes manipular valor.Value según necesites
+            }
+            
+            // Notificar finalización usando el evento de instancia
+            OnComplete?.Invoke();
+            Console.WriteLine($"Ha terminado: {text}");
+        }
+
+        public void Join()
+        {
+            hilo.Join();
+        }
+    }
---
+++ Hilos/Hilos/Wrapper.cs
@@ -0,0 +1,33 @@
+﻿namespace Hilos;
+
+public class RefWrapper<T> where T : struct
+{
+    private T _value;
+
+    public RefWrapper(T initialValue)
+    {
+        _value = initialValue;
+    }
+
+    public T Value
+    {
+        get { return _value; }
+        set { _value = value; }
+    }
+
+    // Operadores implícitos para facilitar el uso
+    public static implicit operator T(RefWrapper<T> wrapper)
+    {
+        return wrapper._value;
+    }
+
+    public static implicit operator RefWrapper<T>(T value)
+    {
+        return new RefWrapper<T>(value);
+    }
+
+    public override string ToString()
+    {
+        return _value.ToString();
+    }
+}
